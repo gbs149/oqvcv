03e52a09718d0614f1f3546ed3445fbe0e56e519
Gabriel Schmitt <gbs149@outlook.com>
2016-09-02 16:52:26 -0300

    SAIDA: alterna entre duas arrays

--- public/saida/js/output.js
+++ public/saida/js/output.js
@@ -2,50 +2,101 @@ $(document).ready(function () {
     "use strict";
 
     var url = "http://192.168.33.10/src/listar-aprovadas.php";
-    var descricoes = [];
-    // para teste
-    // var descricoes = ["Primeira descrição", "Segunda descrição", "Terceira descrição", "Etcetera"];
+
+    var descricoes = ["1", "2", "3", "4", "5", "6", "7", "8"],
+        descricoesPre = ["a", "b", "c", "d"];
+
     var sec = 1000, min = 60000;
 
 
-    // função recursiva para percorrer array com descrições repetidamente.
-    function loopRepeat(array) {
 
-        // se array estiver vazia reiniciar função com array descrições
-        if (array.length === 0) {
-            loopRepeat(descricoes);
-        } else {
-            // copia array para dados para não apagar as descrições
-            var dados = array.slice();
+    // função recursiva para percorrer array com descrições repetidamente,
+    // alternando entre descrições do público e descrições pré-definidas.
+    function loopRepeat(dadosArray, contador) {
+        // variável que define a cada quantas descrições alterna.
+        var alt = 9;
+
+        if (contador % alt !== 0) {
+
+            contador++;
 
             // retira o primeiro string de dados e usa como texto para reprodução e parágrafo
-            var texto = dados.shift();
+            var texto = dadosArray[0].shift();
+            console.log(texto);
             $("#descricao").text(texto);
 
-            // função de síntese de voz.
-            var synth = window.speechSynthesis;
-            var utterThis = new SpeechSynthesisUtterance(texto);
+            speakText(texto, function() {
+                loopRepeat(getArrays(dadosArray), contador);
+            });
 
-            utterThis.pitch = 1.2;
-            utterThis.rate = 1.4;
-            utterThis.lang = "pt-BR";
-            synth.speak(utterThis);
+        } else {
 
-            // ao final do texto, chama recursivamente a função com dados (que é array a partir do segundo elemento)
-            utterThis.onend = function () {
-                setTimeout(function () {
-                    loopRepeat(dados);
-                }, 2000) // intervalo entre uma descrição e outra
-            };
+            // retira o primeiro string de dados e usa como texto para reprodução e parágrafo
+            var texto = dadosArray[1].shift();
+            console.log(texto);
+            $("#descricao").text(texto);
+
+            speakText(texto, function() {
+                loopRepeat(getArrays(dadosArray), 1);
+            });
+
+        }
+
+    }
+
+
+    // função de síntese de voz.
+    function speakText(texto, callback) {
+        var intervalo = 2000;
+
+        var synth = window.speechSynthesis;
+        var utterThis = new SpeechSynthesisUtterance(texto);
+
+        utterThis.pitch = 1.2;
+        utterThis.rate = 1.4;
+        utterThis.lang = "pt-BR";
+
+        synth.speak(utterThis);
+
+        // ao final do texto, chama uma callback
+        utterThis.onend = function () {
+            setTimeout(function () {
+                callback();
+            }, intervalo) // intervalo entre uma descrição e outra
         }
     }
 
+
+    // função para verificar se as arrays estão vazias.
+    // recebe uma array com duas arrays, recupera cópias das arrays de dados se estiverem vazias
+    // ou retorna cópias se não estiverem
+    function getArrays (array) {
+        var newArray1, newArray2;
+        if (array[0].length === 0 && array[1].length === 0) {
+            newArray1 = descricoes.slice();
+            newArray2 = descricoesPre.slice();
+        } else if (array[0].length === 0 && array[1].length > 0) {
+            newArray1 = descricoes.slice();
+            newArray2 = array[1].slice();
+        } else if (array[0].length > 0 && array[1].length === 0) {
+            newArray1 = array[0].slice();
+            newArray2 = descricoesPre.slice();
+        } else if (array[0].length > 0 && array[1].length > 0) {
+            newArray1 = array[0].slice();
+            newArray2 = array[1].slice();
+        }
+        return [newArray1, newArray2];
+    }
+
+
+
     // PARA TESTE
-    // inicia o programa com array descricoes
-    // loopRepeat(descricoes);
+    // inicia o programa com descricoes
+    loopRepeat(getArrays([descricoes, descricoesPre]), 1);
 
 
 
+/*
     // inicia o programa fazendo GET
     $.get(url, function (data) {
         descricoes = data;
@@ -61,5 +112,5 @@ $(document).ready(function () {
             descricoes = data;
         });
     }, 20 * min);
-
+*/
 });
