979065baf9deb26efa3694165947863add8c28db
Gabriel Schmitt <gbs149@outlook.com>
2016-09-05 21:50:26 -0300

    SAIDA: shuffle arrays

--- public/saida/js/output.js
+++ public/saida/js/output.js
@@ -3,41 +3,43 @@ $(document).ready(function () {
 
     var url = "/src/listar-aprovadas.php";
 
-    var descricoes = [],
-        descricoesPre = ["número 1", "número 2", "número 3", "número 4"];
+    var descricoesDoPublico = [],
+        descricoesPreescritas = ["número 1", "número 2", "número 3", "número 4"];
 
-    var sec = 1000, min = 60000;
+    const SEGUNDO = 1000, MINUTO = 60000;
 
 
 
     // função recursiva para percorrer array com descrições repetidamente,
     // alternando entre descrições do público e descrições pré-definidas.
-    function loopRepeat(dadosArray, contador) {
+    function loopRepeat(arraysDescricoes, contador) {
         // variável que define a cada quantas descrições alterna.
-        var alt = 9;
+        var descricaoPreACada = 9;
+        var descricoesPublico = arraysDescricoes[0],
+            descricoesPre = arraysDescricoes[1];
 
-        if (contador % alt !== 0) {
+        if (contador % descricaoPreACada !== 0) {
 
             contador++;
 
             // retira o primeiro string de dados e usa como texto para reprodução e parágrafo
-            var texto = dadosArray[0].shift();
+            var texto = descricoesPublico.shift();
             console.log(texto);
             $("#descricao").text(texto);
 
-            speakText(texto, function() {
-                loopRepeat(getArrays(dadosArray), contador);
+            speakText(texto, function () {
+                loopRepeat(getArrays(arraysDescricoes), contador);
             });
 
         } else {
 
             // retira o primeiro string de dados e usa como texto para reprodução e parágrafo
-            var texto = dadosArray[1].shift();
+            var texto = descricoesPre.shift();
             console.log(texto);
             $("#descricao").text(texto);
 
-            speakText(texto, function() {
-                loopRepeat(getArrays(dadosArray), 1);
+            speakText(texto, function () {
+                loopRepeat(getArrays(arraysDescricoes), 1);
             });
 
         }
@@ -47,19 +49,23 @@ $(document).ready(function () {
 
     // função de síntese de voz.
     function speakText(texto, callback) {
-        var intervalo = 2000;
+        var intervalo = 2 * SEGUNDO;
 
         var synth = window.speechSynthesis;
-        var utterThis = new SpeechSynthesisUtterance(texto);
+        var textToSpeak = new SpeechSynthesisUtterance(texto);
 
-        utterThis.pitch = 1.2;
-        utterThis.rate = 1.4;
-        utterThis.lang = "pt-BR";
+        textToSpeak.volume = 1;
+        textToSpeak.pitch = 1.2;
+        textToSpeak.rate = 1.4;
+        textToSpeak.lang = "pt-BR";
 
-        synth.speak(utterThis);
+        synth.speak(textToSpeak);
 
         // ao final do texto, chama uma callback
-        utterThis.onend = function () {
+        textToSpeak.onend = function () {
+
+            synth.cancel();
+
             setTimeout(function () {
                 callback();
             }, intervalo) // intervalo entre uma descrição e outra
@@ -70,17 +76,17 @@ $(document).ready(function () {
     // função para verificar se as arrays estão vazias.
     // recebe uma array com duas arrays, recupera cópias das arrays de dados se estiverem vazias
     // ou retorna cópias se não estiverem
-    function getArrays (array) {
+    function getArrays(array) {
         var newArray1, newArray2;
         if (array[0].length === 0 && array[1].length === 0) {
-            newArray1 = descricoes.slice();
-            newArray2 = descricoesPre.slice();
+            newArray1 = descricoesDoPublico.slice();
+            newArray2 = descricoesPreescritas.slice();
         } else if (array[0].length === 0 && array[1].length > 0) {
-            newArray1 = descricoes.slice();
+            newArray1 = descricoesDoPublico.slice();
             newArray2 = array[1].slice();
         } else if (array[0].length > 0 && array[1].length === 0) {
             newArray1 = array[0].slice();
-            newArray2 = descricoesPre.slice();
+            newArray2 = descricoesPreescritas.slice();
         } else if (array[0].length > 0 && array[1].length > 0) {
             newArray1 = array[0].slice();
             newArray2 = array[1].slice();
@@ -89,32 +95,46 @@ $(document).ready(function () {
     }
 
 
-/*
+    function shuffleArray(array) {
+
+        var i, j, x;
+
+        for (i = array.length; i; i--) {
+            j = Math.floor(Math.random() * i);
+            x = array[i - 1];
+            array[i - 1] = array[j];
+            array[j] = x;
+        }
+
+        return array;
+    }
+
+
     // PARA TESTE
     // inicia o programa com descricoes
-    loopRepeat(getArrays([descricoes, descricoesPre]), 1);
+    // loopRepeat(getArrays([descricoesPreescritas, descricoesPreescritas]), 1);
 
-*/
 
 
 
     // inicia o programa fazendo GET
-    $.get(url, function (data) {
-        descricoes = data;
-        // inicia o loop
-        loopRepeat(getArrays([descricoes, descricoesPre]), 1);
-    });
+    function iniciar() {
+        $.get(url, function (data) {
+            descricoesDoPublico = shuffleArray(data);
+            // inicia o loop
+            loopRepeat(getArrays([descricoesDoPublico, descricoesPreescritas]), 1);
+        });
+    }
 
-
-
+    iniciar();
 
 
     // timer para atualizar descricoes a cada 20 minutos
     var timer = setInterval(function () {
         $.get(url, function (data) {
-            descricoes = data;
+            descricoesDoPublico = shuffleArray(data);
         });
-    }, 20 * min);
+    }, 20 * MINUTO);
 
 
 });
