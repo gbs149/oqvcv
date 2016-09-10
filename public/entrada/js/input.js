d8452ab83c1c8af3ff16a9e5398503337f86c609
Gabriel Schmitt <gbs149@outlook.com>
2016-09-10 17:31:23 -0300

    envia imediatamente ou guarda na fila

--- public/entrada/js/input.js
+++ public/entrada/js/input.js
@@ -5,11 +5,12 @@ $(document).ready(function () {
         // uma array de strings com as descrições
         // agora só estamos recebendo as descrições.
     ];
+
     var $descricao = $("#descricao"),
         $contador = $("#contador"),
         $confirmacao = $("#confirmacao");
 
-    function enviar(event) {
+    /*function enviar(event) {
         event.preventDefault();
 
         if ($descricao.val()) {
@@ -26,44 +27,91 @@ $(document).ready(function () {
 
             // por enquanto deixamos assim, mas seria mais legal uma mensagem bonitinha na tela que ficasse por uns 5 segundos e desaparecesse
             // alert("Sua mensagem foi adicionada. \nObrigado!");
-            $confirmacao.fadeIn(300, function() {
-                setTimeout(function() {
+            $confirmacao.fadeIn(300, function () {
+                setTimeout(function () {
                     $confirmacao.fadeOut(400);
-                }, 4000);
+                }, 3000);
             });
         }
-    };
+    };*/
+
+    function enviarDescricoes(event) {
+        event.preventDefault();
+
+        // pega o texto do formulário
+        var textoDescricao = $descricao.val();
+        var umaDescricao = [];
+
+        if (textoDescricao) {
+
+            umaDescricao.push(textoDescricao);
+
+            // transforma o objeto json em string para o envio
+            var textMessages = JSON.stringify(umaDescricao);
+            console.log(textMessages);
+
+            // limpa os campos do formulário
+            $descricao.val("");
+            $descricao.focus();
+
+            // reseta o contador
+            $contador.text(300);
+
+            // aciona mensagem de confirmação
+            $confirmacao.fadeIn(300, function () {
+                setTimeout(function () {
+                    $confirmacao.fadeOut(400);
+                }, 3000);
+            });
+
+            // faz o post da mensagem. Se falhar, guarda a descrição para mandar depois.
+            $.post("/src/inserir.php", {
+                mensagens: textMessages
+            },
+                function (data) {
+                    if (data !== "sucesso") {
+                        MENSAGENS.push(textoDescricao);
+                    }
+                });
+        }
+    }
 
     // seta o timer de envio das mensagens
     var TIMER = setInterval(function () {
 
-        // transforma o objeto json em string para o envio
-        var textMessages = JSON.stringify(MENSAGENS);
-        console.log(textMessages);
+        if (MENSAGENS) {
 
-        // faz o post das mensagens
-        $.post("/src/inserir.php", {
-            mensagens: textMessages
-        },
-            // se sucesso, limpa a variável global MENSAGENS
-            // senão deixa por isso mesmo e ela , automaticamente, será reenviada (tentativa de) após 30 minutos
-            function (data) {
-                if (data === "sucesso") {
-                    MENSAGENS = [];
-                }
-            });
-    }, 10 * 60000); // = 10min
+
+            // transforma o objeto json em string para o envio
+            var textMessages = JSON.stringify(MENSAGENS);
+            console.log(textMessages);
+
+            // faz o post das mensagens
+            $.post("/src/inserir.php", {
+                mensagens: textMessages
+            },
+                // se sucesso, limpa a variável global MENSAGENS
+                // senão deixa por isso mesmo e ela , automaticamente, será reenviada (tentativa de) após 30 minutos
+                function (data) {
+                    if (data === "sucesso") {
+                        MENSAGENS = [];
+                    }
+                });
+        }
+    }, 2 * 60000); // = 2min
 
 
 
     // click no botão envia a descrição
-    $("#enviar").on("click", enviar);
+    $("#enviar").on("click", enviarDescricoes);
 
     // contador de caracteres restantes
-    $descricao.keyup(function() {
+    $descricao.keyup(function () {
         $contador.text(300 - $descricao.val().length);
     });
 
+
+    // entrar em tela cheia ao iniciar
     $("#enter-fullscreen").on("click", enterFullScreen);
 
     function enterFullScreen() {
